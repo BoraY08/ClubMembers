5c45206fb4c39bae8c61ccc5514882d7c64f748c
BoraY08 <boray@nycstudents.net>
2025-05-03 23:17:55 +0000

    done

--- ClubMembers.java
+++ ClubMembers.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 public class ClubMembers {
     private ArrayList<MemberInfo> memberList;
-
+    public ClubMembers() {
+        memberList = new ArrayList<MemberInfo>();
+    }
+    public ArrayList<MemberInfo> getMemberList(){
+        return memberList;
+    }
 
     /**
      * Adds new club members to memberList, as described in part (a).
      * Precondition: names is a non-empty array.
      */
     public void addMembers(String[] names, int gradYear) {
-        /* to be implemented in part (a) */ }
+        for (String s: names){
+            memberList.add(new MemberInfo(s, gradYear, true));
+        }
+    }
 
 
     /**
@@ -16,7 +25,23 @@ public class ClubMembers {
      * and are in good standing, as described in part (b).
      */
     public ArrayList<MemberInfo> removeMembers(int year) {
-        /* to be implemented in part (b) */ }
+        ArrayList<MemberInfo> alumni = new ArrayList<MemberInfo>();
+        for (int i = 0; i < memberList.size(); i++){
+            MemberInfo member = memberList.get(i);
+            if (member.getGradYear() <= year){
+                if (member.inGoodStanding()){
+                    alumni.add(member);
+                    memberList.remove(i);
+                    i--;
+        
+                }
+            }
+        }
+        return alumni;
+    }
+    public void setMemberList(ArrayList<MemberInfo> members){
+        memberList = members;
+    }
     // There may be instance variables, constructors, and methods that are not
     // shown.
 }
